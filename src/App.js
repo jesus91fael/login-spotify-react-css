4874140c64757a35f3b8071408e54c6dd413a2bf
Rafael Jesus do Prado <rafaeljprado@gmail.com>
2021-07-14 16:54:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Logo from './logo.png';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <img src={Logo} alt="spotify" width="68px" />
+        <span className="title">Spotify</span>
+      </div>
+      <nav className="menu">
+        <ul>
+          <li><a href="#">SIGN IN</a></li>
+          <li><a href="#">SIGN UP</a></li>
+        </ul>
+      </nav> 
+      <form>
+        <div className="row">
+          <input className="input" type="text" name="username" placeholder="Username" required />
+        </div>
+        <div className="row">
+          <input className="input"type="password" name="password" placeholder="Password" required />
+        </div>
+        <div className="stay">
+          <input type="checkbox" name="signed" id="signed" /> <label htmlFor="signed">stay signed in</label>
+        </div>
+        <div className="row">
+          <button className="button" type="submit">SIGN IN</button>
+        </div>
+        <div className="forgot">
+          <a href="#">Forgot Password?</a>
+        </div>
+      </form>
     </div>
   );
 }
